566687dcbf47598f7bf821771535d720906ee98f
Akshay Suresh <akshays19294@gmail.com>
2020-03-06 10:08:17 +0530

    ADDED: onelogin pythonsaml

---
+++ README.md
@@ -0,0 +1,23 @@
+# sso-test-server
+
+[![Build Status](https://travis-ci.org/The owner of the repository. Either your github username or organization name./sso-test-server.svg?branch=master)](https://travis-ci.org/The owner of the repository. Either your github username or organization name./sso-test-server)
+[![Built with](https://img.shields.io/badge/Built_with-Cookiecutter_Django_Rest-F7B633.svg)](https://github.com/agconti/cookiecutter-django-rest)
+
+Its all about a Weissman score > 5.0. Check out the project's [documentation](http://The owner of the repository. Either your github username or organization name..github.io/sso-test-server/).
+
+# Prerequisites
+
+- [Docker](https://docs.docker.com/docker-for-mac/install/)  
+
+# Local Development
+
+Start the dev server for local development:
+```bash
+docker-compose up
+```
+
+Run a command inside the docker container:
+
+```bash
+docker-compose run --rm web [command]
+```
---
+++ piedpiper/users/views.py
@@ -0,0 +1,52 @@
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
+from .models import User
+from .permissions import IsUserOrReadOnly
+from .serializers import CreateUserSerializer, UserSerializer
+
+from rest_framework.response import Response
+
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
+    """
+    Updates and retrieves user accounts
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsUserOrReadOnly,)
+
+
+class UserCreateViewSet(mixins.CreateModelMixin,
+                        viewsets.GenericViewSet):
+    """
+    Creates user accounts
+    """
+    queryset = User.objects.all()
+    serializer_class = CreateUserSerializer
+    permission_classes = (AllowAny,)
+
+
+class IndexViewset(viewsets.ViewSet):
+    
+    permission_classes = [AllowAny]
+
+    def list(self, request):
+        return Response({ 'message': 'index' })
+
+
+class AttrsViewset(viewsets.ViewSet):
+    
+    permission_classes = [AllowAny]
+
+    def list(self, request):
+        return Response({ 'message': 'attrs' })
+
+
+
+class MetadataViewset(viewsets.ViewSet):
+    
+    permission_classes = [AllowAny]
+
+    def list(self, request):
+        return Response({ 'message': 'metadata' })
