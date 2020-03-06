93360acc417f8413a8c926e3578adcc668704468
Akshay Suresh <akshays19294@gmail.com>
2020-03-06 10:53:07 +0530

    ADDED: settings

--- piedpiper/urls.py
+++ piedpiper/urls.py
@@ -5,17 +5,36 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
-from .users.views import UserViewSet, UserCreateViewSet, MetadataViewset, IndexViewset, AttrsViewset
+from .users.views import UserViewSet, UserCreateViewSet
+# , MetadataViewset, IndexViewset, AttrsViewset
+
+from django.conf.urls import url
+
+import django_saml2_auth.views
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'users', UserCreateViewSet)
 
-router.register(r'index', IndexViewset, basename='index')
-router.register(r'attrs', AttrsViewset, basename='attrs')
-router.register(r'metadata', MetadataViewset, basename='metadata')
+# router.register(r'index', IndexViewset, basename='index')
+# router.register(r'attrs', AttrsViewset, basename='attrs')
+# router.register(r'metadata', MetadataViewset, basename='metadata')
 
 urlpatterns = [
+    # These are the SAML2 related URLs. You can change "^saml2_auth/" regex to
+    # any path you want, like "^sso_auth/", "^sso_login/", etc. (required)
+    url(r'^saml2_auth/', include('django_saml2_auth.urls')),
+
+    # The following line will replace the default user login with SAML2 (optional)
+    # If you want to specific the after-login-redirect-URL, use parameter "?next=/the/path/you/want"
+    # with this view.
+    url(r'^accounts/login/$', django_saml2_auth.views.signin),
+
+    # The following line will replace the admin login with SAML2 (optional)
+    # If you want to specific the after-login-redirect-URL, use parameter "?next=/the/path/you/want"
+    # with this view.
+    url(r'^admin/login/$', django_saml2_auth.views.signin),
+
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
